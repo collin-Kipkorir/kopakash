3d859461e4b93045b30803bf2d01ee92594903b0
collin-Kipkorir <collinkipkorir@gmail.com>
2024-10-15 11:46:43 +0300

    PWA login

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ self.addEventListener('install', (event) => {
     caches.open('v1').then((cache) => {
       return cache.addAll([
         '/',
+        '/index.html',
         '/pwa-login.html',
         '/styles.css',
         '/app.js',
@@ -10,7 +11,10 @@ self.addEventListener('install', (event) => {
         '/manifest.json',
         '/images/icon-192x192.png',
         '/images/icon-512x512.png'
-      ]);
+      ]).catch(error => {
+        console.error('Cache addAll error:', error);
+        return Promise.reject(error);
+      });
     })
   );
 });
@@ -18,8 +22,21 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open('v1').then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    }).catch(error => {
+      console.error('Fetch error:', error);
     })
   );
 });
-
