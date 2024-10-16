80b9bbe32e604d16ba327f05953480ef962c7d40
collin-Kipkorir <collinkipkorir@gmail.com>
2024-10-16 16:34:20 +0300

    app

--- sw.js
+++ sw.js
@@ -1,40 +1,45 @@
+const CACHE_NAME = 'kopa-kash-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './pwa-login.html',
+  './pwa-home.html',
+  './loan-application.html',
+  './pwa-increase.html',
+  './styles.css',
+  './pwa-app.js',
+  './manifest.json',
+  './images/icon-192x192.png',
+  './images/icon-512x512.png'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/pwa-login.html',
-        '/pwa-home.html',
-        '/loan-application.html',
-        '/pwa-increase.html',
-        '/styles.css',
-        '/pwa-app.js',
-        '/manifest.json',
-        '/images/icon-192x192.png',
-        '/images/icon-512x512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).then(response => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
           return response;
         }
-        const responseToCache = response.clone();
-        caches.open('v1').then(cache => {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
-      });
-    }).catch(error => {
-      console.error('Fetch error:', error);
-    })
+        return fetch(event.request).then(
+          (response) => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
   );
 });
